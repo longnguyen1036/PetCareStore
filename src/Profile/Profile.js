77de9927777dec39214ee6e960c61acfa2dd8eec
laubata2000 <laubacho2000@gmail.com>
2023-03-06 16:35:58 +0700

    laubatacode 2man hinh
    gagscgad

---
+++ src/Profile/Profile.js
@@ -0,0 +1,121 @@
+import { Image, StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+import Block from '../components/Block'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+
+const Profile = () => {
+  return (
+    <View style = {{alignItems: 'center', backgroundColor: '#dcdcdc', flex: 1}}>
+        
+        <View style ={{height: '25%', width: '100%', backgroundColor: '#18A2E1', borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20}}>
+        <Block row={1} alignCenter width={'100%'} justifySpaceBetween paddingHorizontal={10} marginTop={'2%'} >
+            <FontAwesome5 name='chevron-left' size={30} />
+            <Text style = {{ width: '60%', color: 'white', fontSize: 20, fontWeight: 'bold' }}>Tài khoản</Text>
+
+        </Block>
+
+
+        </View>
+        <View style = {{height: '27%', width: '90%', backgroundColor: '#FFFFFF', borderRadius: 10,
+    marginTop: '-25%'}}>
+            <View>
+                <View style = {{flexDirection: 'row', paddingHorizontal: 10, paddingVertical: 10}}>
+                    <View>
+                        <Image source={require('../assets/image/profileavatar.png')} style = {{}} ></Image>
+                    </View>
+                    <View style = {{marginLeft: 10, justifyContent: 'center'}}>
+                        <Text style= {{fontSize: 20, fontWeight: '700', color: 'black'}}>phuocps19167</Text>
+                        <Text style= {{marginTop: 10}}>phuocphps19167@fpt.edu.vn</Text>
+                        <Text style= {{marginTop: 5}}>Hoàng Diệu - P Linh Trung - Thủ Đức</Text>
+                    </View>
+                    
+                    
+                </View>
+
+
+            </View>
+            <View style= {{flexDirection: 'row', justifyContent: 'space-around', marginTop: '1%'}}>
+                <TouchableOpacity style={{backgroundColor: '#18A2E1', width: '30%', flexDirection: 'row',justifyContent: 'space-around',
+            alignItems: 'center', padding: 10, borderRadius: 10}}>
+                    <Text style= {{color: 'white', fontWeight: 'bold'}}>Camera</Text>
+                    <FontAwesome5 name='camera-retro' size={25} color ={'white'} />
+                    
+                </TouchableOpacity>
+                <TouchableOpacity style={{backgroundColor: '#18A2E1', width: '30%', flexDirection: 'row',justifyContent: 'space-around',
+            alignItems: 'center', padding: 10, borderRadius: 10}}>
+                    <Text style= {{color: 'white', fontWeight: 'bold'}}>Chat</Text>
+                    <FontAwesome5 name='sms' size={25} color ={'white'} />
+                    
+                </TouchableOpacity>
+
+            </View>
+
+        </View>
+        
+
+        <View style = {{ width: '90%', marginTop: '3%', flexDirection: 'row',
+    }}>
+            <TouchableOpacity style={{backgroundColor: '#18A2E1', padding: 8, borderRadius: 8}}>
+                <FontAwesome5 name='stream' size={30} color={'white'} />
+            </TouchableOpacity>
+            <View style={{marginLeft: '3%'}}>
+                <Text style={{fontSize: 18, color: 'black', fontWeight: 'bold'}}>Chỉnh sửa tài khoản</Text>
+                <Text>Chỉnh sửa và quản lý tài khoản của bạn</Text>
+            </View>
+
+        </View>
+
+        <View style = {{ width: '90%', marginTop: '3%', backgroundColor: '#18A2E1', height: '18%',
+    borderRadius: 8, alignItems: 'center', justifyContent: 'center'}}>
+            <TouchableOpacity style ={{flexDirection: 'row', justifyContent: 'space-around', paddingHorizontal: 10}}>
+                <Text style={{width: '90%', fontSize: 16, color: 'white', fontWeight: 'bold'}}>Thay đổi mật khẩu</Text>
+                <FontAwesome5 style={{width: '10%'}} name='chevron-right' size={18} />
+            </TouchableOpacity>
+            <View style={{borderWidth: 0.5, borderColor: 'white', width: '95%', marginTop: '3%'}}></View>
+            <TouchableOpacity style ={{flexDirection: 'row', justifyContent: 'space-around', paddingHorizontal: 10, marginTop: '3%'}}>
+                <Text style={{width: '90%', fontSize: 16, color: 'white', fontWeight: 'bold'}}>Thay địa chỉ</Text>
+                <FontAwesome5 style={{width: '10%'}} name='chevron-right' size={18} />
+            </TouchableOpacity>
+
+        </View>
+        <View style = {{ width: '90%', marginTop: '3%', flexDirection: 'row',
+    }}>
+            <TouchableOpacity style={{backgroundColor: '#18A2E1', padding: 8, borderRadius: 8}}>
+                <FontAwesome5 name='comments' size={30} color={'white'} />
+            </TouchableOpacity>
+            <View style={{marginLeft: '3%'}}>
+                <Text style={{fontSize: 18, color: 'black', fontWeight: 'bold'}}>Hỗ trợ và phản hồi</Text>
+                <Text>Liên hệ với chúng tôi</Text>
+            </View>
+
+        </View>
+        
+        
+        <View style = {{ width: '90%', marginTop: '3%', backgroundColor: '#18A2E1', height: '22%',
+    borderRadius: 8, alignItems: 'center', justifyContent: 'center'}}>
+            <TouchableOpacity style ={{flexDirection: 'row', justifyContent: 'space-around', paddingHorizontal: 10}}>
+                <Text style={{width: '90%', fontSize: 16, color: 'white', fontWeight: 'bold'}}>Lịch sử mua hàng</Text>
+                <FontAwesome5 style={{width: '10%'}} name='chevron-right' size={18} color={'white'} />
+            </TouchableOpacity>
+            <View style={{borderWidth: 0.5, borderColor: 'white', width: '95%', marginTop: '3%'}}></View>
+            <TouchableOpacity style ={{flexDirection: 'row', justifyContent: 'space-around', paddingHorizontal: 10, marginTop: '3%'}}>
+                <Text style={{width: '90%', fontSize: 16, color: 'white', fontWeight: 'bold'}}>Lịch sử đặt dịch vụ</Text>
+                <FontAwesome5 style={{width: '10%'}} name='chevron-right' size={18} color={'white'} />
+            </TouchableOpacity>
+            <View style={{borderWidth: 0.5, borderColor: 'white', width: '95%', marginTop: '3%'}}></View>
+            <TouchableOpacity style ={{flexDirection: 'row', justifyContent: 'space-around', paddingHorizontal: 10, marginTop: '3%'}}>
+                <Text style={{width: '90%', fontSize: 16, color: 'white', fontWeight: 'bold'}}>Đăng xuất</Text>
+                <FontAwesome5 style={{width: '10%'}} name='chevron-right' size={18} color={'white'} />
+            </TouchableOpacity>
+
+        </View>
+        
+      
+    </View>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
---
+++ src/screen/Home.js
@@ -0,0 +1,140 @@
+import { ScrollView, StyleSheet, Text, View , Image, TouchableOpacity, FlatList} from 'react-native'
+import React from 'react'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import { SliderBox } from "react-native-image-slider-box";
+import Block from '../components/Block'
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+
+const DATA = [
+    {
+      id: 1,
+      name: 'BEAGLE CƯNG CƯNG',
+      price: 1800000,
+      images: require('../assets/image/salad.png'),
+    },
+    {
+      id: 2,
+      name: 'BEAGLE CƯNG CƯNG',
+      price: 2000000,
+      images: require('../assets/image/salad.png'),
+    },
+  ];
+
+  const renderItem = ({item}) => {
+    return (
+      <Block
+        marginLeft={'8%'}
+        backgroundColor={'#E6EAED'}
+        width={150}
+        height={168}>
+        <Image style={styles.ilist} source={item.images}></Image>
+        <Block paddingLeft={'5%'} margin={5} backgroundColor={'white'} height={70}>
+          <Block paddingTop={5}>
+            <Text>{item.name}</Text>
+            <Text marginTop={7} size={12}>{item.price} VND</Text>
+          </Block>
+          <TouchableOpacity style={styles.nut}>
+            <AntDesign name="right" size={25} />
+          </TouchableOpacity>
+        </Block>
+      </Block>
+    );
+  };
+
+const Home = () => {
+    const images = [
+        require('../assets/image/anhcamnang.png'),
+        require('../assets/image/anhcamnang1.png'),
+        require('../assets/image/caphe.png'),
+        require('../assets/image/main.png'),
+        require('../assets/image/salad.png'),
+
+    ];
+  return (
+    <View style={{flex: 1, backgroundColor: '#F5F5F5'}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: '5%', marginTop: '3%'}}>
+        <View>
+            <Text style ={{fontSize: 16, fontWeight: '700', color: '#18A2E1'}}>Welcome to</Text>
+            <Text style ={{fontSize: 22, fontWeight: '700', color: 'black'}}>Tên tài khoản</Text>
+
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+            <FontAwesome5 style={{marginRight: '15%'}} color={'black'} name='lock' size={25} />
+            <TouchableOpacity>
+                <Image source={require('../assets/image/profiledog.png')}/>
+            </TouchableOpacity>
+            
+
+        </View>
+
+      </View>
+
+      <View style={{marginTop: '10%'}}>
+        <SliderBox 
+            images={images}
+            dotColor="#FFEE58"
+            inactiveDotColor="#90A4AE"
+            autoplay = {true}
+            // autoplayInterval={1000}
+            circleLoop={true}
+            ImageComponentStyle={{borderRadius: 15, width: '90%', marginTop: 5}}
+        />
+
+      </View>
+
+      <View style={{paddingHorizontal: '5%', marginTop: '5%'}}>
+        <Text style ={{fontSize: 20, fontWeight: '700', color: 'black'}}>Danh mục</Text>
+      </View>
+
+      <View style ={{flexDirection: 'row', justifyContent: 'space-around', marginTop: '3%'}}>
+        <TouchableOpacity style={{backgroundColor: '#18A2E1', padding: 10, borderRadius: 8}}>
+            <FontAwesome5  color={'black'} name='paw' size={25} />
+
+        </TouchableOpacity>
+        <TouchableOpacity style={{backgroundColor: '#18A2E1', padding: 10, borderRadius: 8}}>
+            <FontAwesome5  color={'black'} name='clock' size={25} />
+
+        </TouchableOpacity>
+        <TouchableOpacity style={{backgroundColor: '#18A2E1', padding: 10, borderRadius: 8}}>
+            <FontAwesome5  color={'black'} name='toolbox' size={25} />
+
+        </TouchableOpacity>
+      </View>
+      <View style={{paddingHorizontal: '5%', marginTop: '3%'}}>
+        <Text style ={{fontSize: 20, fontWeight: '700', color: 'black'}}>Top bán chạy</Text>
+      </View>
+      <View style ={{marginTop: '3%'}}>
+        <FlatList
+        numColumns={2}
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+        />
+
+      </View>
+
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    ilist: {
+        width: 80,
+        height: 90,
+        marginLeft: '20%',
+      },
+      nut: {
+        width: 32,
+        height: 32,
+        backgroundColor: '#F2F3F2',
+        position: 'absolute',
+        right: '5%',
+        bottom: '8%',
+        alignItems: 'center',
+        borderRadius: 4,
+        paddingTop: '15%',
+      },
+})
