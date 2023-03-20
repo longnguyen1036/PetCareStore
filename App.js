dc4992f06976e4a9d00f47daa85b13444e569576
toancodedao <dominhtoan02@gmail.com>
2023-03-20 21:04:02 +0700

    e

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {
-  ConfirmOTP,
-  CreateNewPass,
-  ForgetPass,
-  Login,
-  Register,
-  Welcome,
-} from './src/screen/Auth';
 
 import { MyTab } from './src/router/BottomNavigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
