cabd2a16774298e7fb5ea34d2aab064f496c9919
toancodedao <dominhtoan02@gmail.com>
2023-03-20 21:24:03 +0700

    toan

--- src/screen/Profile/Profile.js
+++ src/screen/Profile/Profile.js
@@ -159,6 +159,7 @@ import { loggedAction, logoutAction } from '../../redux/actions/authAction';
             width: '90%',
             marginTop: '3%',
             backgroundColor: '#18A2E1',
+            height: '13%',
             borderRadius: 8,
             alignItems: 'center',
             justifyContent: 'center',
@@ -179,6 +180,7 @@ import { loggedAction, logoutAction } from '../../redux/actions/authAction';
               }}>
               Thay đổi mật khẩu
             </Text>
+            <FontAwesome5 color={'white'} style={{width: '10%'}} name="chevron-right" size={18} />
           </TouchableOpacity>
           <View
             style={{
@@ -204,6 +206,7 @@ import { loggedAction, logoutAction } from '../../redux/actions/authAction';
               }}>
               Thay địa chỉ
             </Text>
+            <FontAwesome5 color={'white'} style={{width: '10%'}} name="chevron-right" size={18} />
           </TouchableOpacity>
         </View>
         <View style={{width: '90%', marginTop: '3%', flexDirection: 'row'}}>
@@ -224,6 +227,7 @@ import { loggedAction, logoutAction } from '../../redux/actions/authAction';
             width: '90%',
             marginTop: '3%',
             backgroundColor: '#18A2E1',
+            height: '18%',
             borderRadius: 8,
             alignItems: 'center',
             justifyContent: 'center',
--- src/screen/Services/ListServices.js
+++ src/screen/Services/ListServices.js
@@ -1,17 +1,147 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  SafeAreaView,
+  FlatList,
+} from 'react-native';
+import React from 'react';
+import Block from '../../components/Block';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {
+  INSERT_PETS_SCREEN,
+  INSERT_SERVICES_SCREEN,
+} from '../../router/ScreenName';
 
 const DATA = [
+  {
+    id: '1',
+    title: 'First Item',
+    price: 800000,
+    price1: 900000,
+    address:
+      'Địa chỉ: 147 Nguyễn Sỹ Sách, phường 15, Tân Bình, thành phố Hồ Chí Minh.',
+    image: require('../../assets/image/profileavatar.png'),
+  },
+  {
+    id: '2',
+    title: 'First Item',
+    price: 700000,
+    price1: 800000,
+    address:
+      'Địa chỉ: 147 Nguyễn Sỹ Sách, phường 15, Tân Bình, thành phố Hồ Chí Minh.',
+    image: require('../../assets/image/profileavatar.png'),
+  },
+  {
+    id: '3',
+    title: 'First Item',
+    price: 600000,
+    price1: 700000,
+    address:
+      'Địa chỉ: 147 Nguyễn Sỹ Sách, phường 15, Tân Bình, thành phố Hồ Chí Minh.',
+    image: require('../../assets/image/profileavatar.png'),
+  },
+  {
     id: '4',
+    title: 'First Item',
+    price: 500000,
+    price1: 600000,
+    address:
+      'Địa chỉ: 147 Nguyễn Sỹ Sách, phường 15, Tân Bình, thành phố Hồ Chí Minh.',
+    image: require('../../assets/image/profileavatar.png'),
+  },
+  {
     id: '5',
+    title: 'First Item',
+    price: 400000,
+    price1: 400000,
+    address:
+      'Địa chỉ: 147 Nguyễn Sỹ Sách, phường 15, Tân Bình, thành phố Hồ Chí Minh.',
+    image: require('../../assets/image/profileavatar.png'),
+  },
+];
 
+const Item = ({title, price, image, price1, address}) => (
+  <View
+    style={{
+      width: '100%',
+      backgroundColor: '#E6EAED',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      paddingVertical: 10,
+      borderRadius: 8,
+      marginTop: '3%',
+    }}>
+    <Block alignCenter marginTop={'2%'}>
+      <Image style={{width: 100, height: 120}} source={image}></Image>
+    </Block>
+    <View
+      style={{
+        backgroundColor: 'white',
+        paddingLeft: '4%',
+        borderRadius: 10
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
 
+        }}>
+        <View>
+          <Text style={{fontSize: 18, color: 'black'}}>{title}</Text>
+          <Text style={{color: 'red'}}>{price}</Text>
+          <Text style={{color: 'black'}}>{price1}</Text>
         </View>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#F2F3F2',
+            borderRadius: 5,
+            width: 30,
+            height: 30, 
+            paddingLeft: '5%',
+            paddingTop: '2%',
+            marginRight: '4%'
+          }}>
+          <FontAwesome5
+            style={{}}
+            color={'black'}
+            name="chevron-right"
+            size={18}
+          />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text>Cửa hàng: Petmart</Text>
+        <Text style={{width: 250, fontSize: 16, color: 'black'}}>
+          {address}
+        </Text>
+      </View>
+    </View>
+  </View>
+);
 
+const ListServices = ({navigation}) => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <FontAwesome5
+            style={{}}
+            color={'black'}
+            name="chevron-left"
+            size={25}
+          />
         </TouchableOpacity>
         <View>
+          <Text style={styles.textHeader}>Dịch vụ</Text>
         </View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(INSERT_SERVICES_SCREEN)}>
+          <FontAwesome5 style={{}} color={'black'} name="plus" size={25} />
         </TouchableOpacity>
       </View>
 
@@ -24,6 +154,12 @@ const DATA = [
           height={40}
           borderRadius={15}
           margin={10}>
+          <FontAwesome5
+            style={styles.seachImage}
+            color={'black'}
+            name="search"
+            size={20}
+          />
           <TextInput
             placeholder="Tìm kiếm"
             style={{flex: 1}}
@@ -31,10 +167,46 @@ const DATA = [
         </Block>
       </Block>
 
+      <SafeAreaView style={{paddingHorizontal: '2%'}}>
         <FlatList
+          data={DATA}
+          renderItem={({item}) => (
+            <Item
+              title={item.title}
+              price={item.price}
+              image={item.image}
+              price1={item.price1}
+              address={item.address}
+            />
+          )}
+          keyExtractor={item => item.id}
         />
       </SafeAreaView>
     </View>
+  );
+};
 
+export default ListServices;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: '5%',
+    paddingVertical: '3%',
+  },
+  textHeader: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: 'black',
+  },
+  seachImage: {
+    padding: 10,
+    marginLeft: 10,
+    alignItems: 'center',
+  },
+});
