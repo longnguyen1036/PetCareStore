09324e95549fd29cd7c676bc950c992c9bd02bd6
toancodedao <dominhtoan02@gmail.com>
2023-03-20 22:20:42 +0700

    t

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -6,7 +6,6 @@ const InsertPet = async (namePet, agePet, typePet, image, pricePet, quantityPet,
     try {
         console.log('log insert pet', namePet, agePet, typePet, image, pricePet, quantityPet, descriptionPet, genderPet, code, nameModel)
         const token = await getToken();
-        console.log('token heheheeh', token);
         const formData = new FormData();
          formData.append('namePet', namePet);
         formData.append('agePet', agePet);
@@ -23,14 +22,35 @@ const InsertPet = async (namePet, agePet, typePet, image, pricePet, quantityPet,
         formData.append('genderPet', genderPet);
         formData.append('code', code);
         formData.append('nameModel', nameModel);
+
+
         console.log('formDatadsds', formData._parts);
-        const insertPet = await axios.post(`${BASE_URL_TEST}/addproduct`, formData._parts, {
+
+
+        // const insertPet = await axios.post(`${BASE_URL_TEST}/addproduct`, {
+        //     headers: {
+        //         token: `Bearer ${token}`
+        //     },data : {
+        //         formData._parts
+        //     }
+        // })
+        
+        const insertPet = {
+            method: 'POST',
+            url: `${BASE_URL_TEST}/addproduct`,
             headers: {
                 token: `Bearer ${token}`
+            }, data: {
+                formData
             }
-        })
-        // đợi tui xem lại code cái
-        
+          };
+          
+          axios.request(insertPet).then(function (response) {
+              console.log(response.data);
+          }).catch(function (error) {
+              console.error(error);
+          });
+          
         console.log('insert thanh cong', insertPet)
         return insertPet
     } catch (error) {
